ceb25ca26a0afe1c080aeb90fac31831c2a8d987
Anatoly Zinovyev <enderforever1337@gmail.com>
2022-01-24 00:55:44 +0300

    initial commit, basic functionality+playground

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include "myshptr.h"
+#include <memory>
+#include <cmath>
+
+struct whatsoever
+{
+    int x;
+    int y;
+    double z;
+    friend std::ostream& operator<<(std::ostream &os, const whatsoever &w)
+    {
+        return os<<w.x<<" "<<w.y<<" "<<w.z;
+    }
+};
+
+//used just to print count.
+
+void prcnt(long sz)
+{
+    std::cout << "count: " << sz << std::endl;
+}
+// here we want to show that in case of passing shared pointer by value we copy it, i.e. increment refcount.
+template <class P>
+void pass_value(test::shared_ptr<P> s)
+{
+    prcnt(s.use_count());
+    std::cout<<*s<<std::endl;
+}
+
+// a little playground instead of depending on test framework.
+
+int main()
+{
+    test::shared_ptr<std::string> brackets{new std::string};
+    *brackets = std::string("brackets");
+    std::cout<<*brackets<<std::endl;
+
+    // nullptr_t
+    {
+        std::string *s = nullptr;
+        test::shared_ptr<std::string> custom(s);
+        std::shared_ptr<std::string> standard(s);
+        test::shared_ptr<std::string> custom_from_null(nullptr);
+        std::shared_ptr<std::string> standard_from_nullptr(nullptr);
+        auto custom_copy = custom;
+        auto standard_copy = standard;
+        auto std_from_nptr_cp = standard_from_nullptr;
+        auto cstm_fr_nptr_cp = custom_from_null;
+
+        std::cout<<"custom use count "<<custom_copy.use_count()<<" "<<cstm_fr_nptr_cp.use_count()<<std::endl;
+        std::cout<<"standard use count"<<standard_copy.use_count()<<" "<<std_from_nptr_cp.use_count()<<std::endl;
+
+
+/*
+        std::cout<<(h.get()==z.get());
+        if(h) std::cout<<"null!"; */
+    }
+    {
+        test::shared_ptr<std::string> s(new std::string()); // make a pointer
+        *s = "amIalive?"; // work with it
+        s->append("!");
+
+        std::vector<test::shared_ptr<std::string>> v(10,s); // spread it to increase refcount
+        prcnt(s.use_count()); // show
+        pass_value(s); // increase once
+        prcnt(s.use_count()); // show decrease
+        s.reset(); // and again
+        for(size_t i = 0; i<9; i++)
+        {
+            prcnt(v[i].use_count());
+            v[i].reset(); // and again
+        }
+        s = std::move(v[9]); // last man standing.
+        prcnt(s.use_count());
+    }
+    {
+        test::shared_ptr<whatsoever>(nullptr); // anonymous
+        pass_value(test::shared_ptr<double>(new double(std::log(15.1542))));
+        // test empty
+        test::shared_ptr<whatsoever> emp;
+        emp.reset();
+        emp.reset(new whatsoever{5,4,3});
+        //test move
+        test::shared_ptr<whatsoever> another(new whatsoever{1,2,3});
+        emp = std::move(another);
+        std::cout<<*emp<<std::endl;
+    }
+    std::cin.get();
+    return 0;
+}
---
+++ myshptr.h
@@ -0,0 +1,134 @@
+#ifndef MYSHPTR_H
+#define MYSHPTR_H
+#include <atomic>
+#include <cstddef>
+
+namespace test
+{
+template <class P>
+class shared_ptr
+{
+    class object_holder
+    {
+        P* m_object;
+        std::atomic<long> m_rcnt;
+    public:
+        explicit object_holder(P* obj) : m_object(obj), m_rcnt(0) {}
+        P* getObject() const
+        {
+            return m_object;
+        }
+        void inc()
+        {
+            ++m_rcnt;
+        }
+        long get_m_rcnt()
+        {
+            return m_rcnt;
+        }
+        void dec()
+        {
+            --m_rcnt;
+        }
+        ~object_holder()
+        {
+            delete m_object;
+        }
+
+    } *m_obj;
+
+public:
+    shared_ptr() noexcept : m_obj(nullptr) {}
+
+    shared_ptr(std::nullptr_t) noexcept : m_obj(nullptr) { std::cout<<"not useless!"<<std::endl; };
+
+explicit shared_ptr (P* pointer) :
+        m_obj(new object_holder(pointer)) {} // may throw bad_alloc
+    ~shared_ptr()
+    {
+        release();
+    }
+    shared_ptr(const shared_ptr& other) noexcept
+    {
+        *this = other;
+    }
+    shared_ptr(shared_ptr&& other) noexcept
+    {
+        *this = std::move(other);
+    }
+    shared_ptr &operator=(const shared_ptr& other) noexcept
+    {
+        if(&other == this)
+        {
+            return *this;
+        }
+        this->m_obj = other.m_obj;
+        if(this->m_obj)
+            this->m_obj->inc();
+        return *this;
+    }
+    shared_ptr &operator=(shared_ptr &&other) noexcept
+    {
+        if(&other == this)
+        {
+            return *this;
+        }
+        this->release();
+        this->m_obj = other.m_obj;
+        other.m_obj = nullptr;
+        return *this;
+
+    }
+    operator bool() const noexcept
+    {
+        if (m_obj)
+        {
+            return m_obj->getObject() != nullptr;
+        }
+        else return false;
+    }
+    size_t use_count() noexcept
+    {
+        if(m_obj)
+            return m_obj->get_m_rcnt()+1;
+        else return 0;
+    }
+    P &operator*() const noexcept
+    {
+        if(m_obj)
+            return *m_obj->getObject();
+    }
+    P *operator->() const noexcept
+    {
+        if(m_obj)
+            return m_obj->getObject();
+    }
+    P *get() const noexcept
+    {
+        if(m_obj)
+            return m_obj->getObject();
+    }
+    void reset() noexcept
+    {
+        *this = shared_ptr();
+    }
+    void reset(P* other)
+    {
+        *this = shared_ptr(other);
+    }
+
+private:
+    void release()
+    {
+        if (m_obj)
+        {
+            m_obj->dec();
+            if(m_obj->get_m_rcnt() == -1)
+                delete m_obj;
+        }
+    }
+
+};
+
+}
+#endif // MYSHPTR_H
